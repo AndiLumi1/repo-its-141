7a180c0403ff6f67eab31a567ae7faaeadfd02ce
AndiLumi1 <andi.lumi.14-1@accademiadigitaleliguria.it>
2025-06-26 16:48:34 +0200

    nuovi test

--- ConsoleApp3/LibCalcolatrice/Calcolatrice.cs
+++ ConsoleApp3/LibCalcolatrice/Calcolatrice.cs
@@ -7,6 +7,21 @@
         {
            return addendo1 + addendo2;
         }
+        public int Differenza(int a, int b)
+        {
+            return a - b;
+        }
 
+        public int Prodotto(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divisione(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Impossibile dividere per zero.");
+            return a / b;
+        }
     }
 }
--- ConsoleApp3/TestCalcolatrice/Test1.cs
+++ ConsoleApp3/TestCalcolatrice/Test1.cs
@@ -12,5 +12,29 @@ namespace TestCalcolatrice
             var risultato = calc.Somma(3, 5);
             Assert.AreEqual(8, risultato);
         }
+
+
+        [TestMethod]
+        public void TestDifferenza()
+        {
+            var calc = new Calcolatrice();
+            var risultato = calc.Somma(10, -2);
+            Assert.AreEqual(8, risultato);
+        }
+        [TestMethod]
+        public void TestProdotto()
+        {
+            var calc = new Calcolatrice();
+            var risultato = calc.Somma(4, 4);
+            Assert.AreEqual(8, risultato);
+        }
+        [TestMethod]
+        public void TestDivisione()
+        {
+            var calc = new Calcolatrice();
+            var risultato = calc.Somma(16, -8);
+            Assert.AreEqual(8, risultato);
+        }
     }
 }
+

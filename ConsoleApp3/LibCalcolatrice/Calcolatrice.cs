0418a47f04ca95ca32fc165a9b592d2bff5b90f9
AndiLumi1 <andi.lumi.14-1@accademiadigitaleliguria.it>
2025-06-26 16:39:06 +0200

    test sommma

---
+++ ConsoleApp3/LibCalcolatrice/Calcolatrice.cs
@@ -0,0 +1,12 @@
+﻿namespace LibCalcolatrice
+{
+    public class Calcolatrice 
+
+    {
+        public int Somma(int addendo1, int addendo2)
+        {
+           return addendo1 + addendo2;
+        }
+
+    }
+}
---
+++ ConsoleApp3/TestCalcolatrice/Test1.cs
@@ -0,0 +1,16 @@
+﻿using LibCalcolatrice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            var calc = new Calcolatrice();
+            var risultato = calc.Somma(3, 5);
+            Assert.AreEqual(8, risultato);
+        }
+    }
+}

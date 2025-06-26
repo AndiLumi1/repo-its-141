8afb4ba17725c72ae23c0a1aa05f3bc1aafd9109
AndiLumi1 <andi.lumi.14-1@accademiadigitaleliguria.it>
2025-06-26 16:06:34 +0200

    nuovo commit 1

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            // This is a simple console application that prints "Hello, World!" to the console.
         }
     }
 }
